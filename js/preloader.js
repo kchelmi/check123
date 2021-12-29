3728b3eb669b427217fc4081d0a40777540782d8
kchelmi <hnwpol@gmail.com>
2021-12-29 13:12:03 +0100

    1234

--- js/preloader.js
+++ js/preloader.js
@@ -2,4 +2,4 @@ const preload = document.querySelector('.preload');
 preload.classList.add('show-preloader');
 window.addEventListener('load', () => {
   preload.classList.remove('show-preloader');
-})
+});
